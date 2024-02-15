7e231849478c9596d6b6e391eb52f933c1802191
gmurad97 <murad.dev@bk.ru>
2024-02-15 07:41:42 +0400

    update php 8.3.3 and nullean laravel

--- routes/web.php
+++ routes/web.php
@@ -13,25 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/dashboard', function () {
-    return view("dashboard");
-})->name("dashboard");
-
-
-
-Route::prefix("/posts")->group(function () {
-    //temp views
-    Route::get('/', function () {
-        return view("post.list");
-    })->name("post.list");
-
-    //temp views
-    Route::get('/create', function () {
-        return view("post.create");
-    })->name("post.create");
-
-    //temp views
-    Route::get('/edit', function () {
-        return view("post.edit");
-    })->name("post.edit");
+Route::get('/', function () {
+    return view('welcome');
 });
