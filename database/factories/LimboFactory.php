8a9f14f9aa320a1b07701869bc8a7b86aaf197fc
gmurad97 <murad.dev@bk.ru>
2024-02-16 20:06:23 +0400

    release

---
+++ database/factories/LimboFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Limbo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class LimboFactory extends Factory{
+    protected $model = Limbo::class;
+
+    public function definition(): array
+    {
+        return [
+            'created_at' => Carbon::now(),//
+'updated_at' => Carbon::now(),
+        ];
+    }
+}
