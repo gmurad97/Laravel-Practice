4f4bc49d82f64e693da83748eee3f1dcbb693b15
gmurad97 <murad.dev@bk.ru>
2024-02-18 04:10:03 +0400

    route convension complete

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//FOR GET REQUEST ALL PRODUCTS [CONTROLLER::index]
+Route::get("/products", function () {
+    return "[GET::Request]" . url("/products");
+});
+
+//FOR GET REQUEST TARGET PRODUCT [CONTROLLER::show]
+Route::get("/products/{id}", function ($id) {
+    return "[GET::Request]" . url("/products/{$id}");
+});
+
+//FOR POST RUQUEST ADD PRODUCTS [CONTROLLER::store]
+Route::post("/products", function () {
+    return "[POST::Request]" . url("/products");
+});
+
+//FOR PUT REQUEST FULLY EDIT PRODUCT [CONTROLLER::update]
+Route::put("/products/{id}", function ($id) {
+    return "[PUT::Request]" . url("/products/{$id}");
+});
+
+//FOR PUT REQUEST PARTLY EDIT PRODUCT [CONTROLLER::update]
+Route::patch("/products/{id}", function ($id) {
+    return "[PATCH::Request]" . url("/products/{$id}");
+});
+
+//FOR DELETE REQUEST DELETE PRODUCT [CONTROLLER::destroy]
+Route::delete("/products/{id}", function ($id) {
+    return "[DELETE::Request]" . url("/products/{$id}");
+});
