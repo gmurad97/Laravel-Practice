79705d29771df88253e0f703e0aee60a35cca341
gmurad97 <murad.dev@bk.ru>
2024-03-07 08:21:37 +0400

    combined method func and use softdelete

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,15 +5,64 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CategoryController extends Controller
 {
     public function index()
     {
+/*         $category = Category::updateOrCreate(
+            [
+                "name" => "updated Cheyenne Romaguera Jr.",
+            ],
+            [
+                "name" => "updated change Cheyenne Romaguera Jr.",
+                "parent_id" => "2",
+                "created_at" => Carbon::now(),
+                "updated_at" => Carbon::now(),
+            ]
 
-/*         $categories = Category::all();
+            //if attribute found data updated another created...
+        );
 
-        dd($categories); */
+        dd($category->created_at->format("Y-m-d H:i:s")); // normalize data->object
+        //devsense php no work for format(intellisense)
+        */
+
+
+
+
+
+
+/*         $category = Category::firstOrCreate(
+            [
+                "name" => "Cheyenne Romaguera Jr",
+            ],
+            [
+                "name" => "new Cheyenne Romaguera Jr.",
+                "parent_id" => "2",
+                "created_at" => Carbon::now(),
+                "updated_at" => Carbon::now(),
+            ]
+        );
+
+        dd($category->created_at->format("Y-m-d H:i:s")); */
+
+
+
+        /*         $category = Category::create([
+                    "name"=> "new category",
+                    "parent_id"=> "2",
+                    "created_at"=> Carbon::now(),
+                    "updated_at"=> Carbon::now(),
+                ]);
+
+                dd($category->name); */
+
+
+        /*         $categories = Category::all();
+
+                dd($categories); */
 
 
 
@@ -25,23 +74,23 @@ class CategoryController extends Controller
         ]); */
 
 
-/*         $category = Category::find(28);
+        /*         $category = Category::find(28);
 
-        $category->update(
-            [
-                "name" => "updated new category",
-                "parent_id" => "2",
-                "created_at" => Carbon::now(),
-                "updated_at" => Carbon::now(),
-            ]
-        ); */
+                $category->update(
+                    [
+                        "name" => "updated new category",
+                        "parent_id" => "2",
+                        "created_at" => Carbon::now(),
+                        "updated_at" => Carbon::now(),
+                    ]
+                ); */
         // |||||||||||||||||||||||||||||||||||||||||||||
 
-/*         $category = Category::find(28);
+        /*         $category = Category::find(28);
 
-        $category->name = "updated new category1";
+                $category->name = "updated new category1";
 
-        $category->save(); */
+                $category->save(); */
 
         ///////////////////////////////////
 
@@ -49,13 +98,19 @@ class CategoryController extends Controller
         $categories->delete(); */
 
 
-/*         $categories = Category::withTrashed()->find(10);
+        /*         $categories = Category::withTrashed()->find(10); // trashed + notrashed element
 
-        dd($categories->id); */
+                dd($categories->id); */
 
-        /* $category = Category::withTrashed()->find(10);
+        /* $categories = Category::find(3);
+        $categories->delete(); */
 
-        $category->restore(); */
+        /*         $categories = Category::onlyTrashed()->get(); //only trashed element
+
+                foreach ($categories as $category) {
+                    dump($category->name);
+                } */
+
     }
 
     public function show($id)
