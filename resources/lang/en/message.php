6c0424164d560e09c2cbf7cd1349b4577d04d0c2
gmurad97 <murad.dev@bk.ru>
2024-03-20 05:37:12 +0400

    multi language see and first key pair sets

---
+++ resources/lang/en/message.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    "word_key" => "word_translate_value",
+    "word_key2" => "word_translate_value :Name", // __('word_key2',["name" => "murad"])
+];
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Middleware\LanguageMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/lang', function () {
+})->middleware(LanguageMiddleware::class);
 
 Route::group(
     [
