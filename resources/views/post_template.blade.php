903ec938b66f1be1f2ce760c13e9f9cd5cf8dbc5
gmurad97 <murad.dev@bk.ru>
2024-04-07 04:59:09 +0400

    ml commit

--- resources/views/post_template.blade.php
+++ resources/views/post_template.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@lang("messages.site_title")</title>
+    <title>{{ __("messages.site_title") }}</title>
+    <title>{{ $localization["lang"] }}</title>
     @vite(["resources/sass/app.scss","resources/js/app.js"])
 </head>
 <body>
