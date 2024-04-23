fe7b53d81558ae85c3bb65bbfa37344fa9a022bd
gmurad97 <murad.dev@bk.ru>
2024-04-23 17:09:44 +0400

    Laravel Documentation - Getting Started (4/6)

--- app/Models/Localization.php
+++ app/Models/Localization.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
+use App\Mail\WelcomeMail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Mail;
 
 class Localization extends Model
 {
     use HasFactory;
     protected $table = "localization";
+
+    public function translations(){
+        Mail::send(new WelcomeMail());
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,9 @@
 <?php
 
-use App\Services\RandomStringService;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/doc", function () {
-    // return App::environment();
-    // Config::
-    return env("APP_ENV");
-});
-
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => App::version()];
 });
 
-Route::get('/post', function () {
-    /*     $data = \App\Models\Localization::where("key","title.name.dynamic")->first();
-        dd($data->en); // $data["en"] */
-    App::setLocale('az');//global set language
-    //session for save memory
-    //or Session facades
-    //Session::put('lang','en');
-    return view("post_template");
-});
-
-Route::get("/rnd", function () {
-    $rndtor = app(RandomStringService::class);
-    return $rndtor->generateRandomString();
-});
+require __DIR__.'/auth.php';
